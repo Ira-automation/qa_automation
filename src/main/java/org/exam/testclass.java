4bbb8bce31a85e2b7810749e85ae1550f3d17447
Iryna Olkhovyk <>
2023-12-04 22:03:30 +0200

    first lecture

--- src/main/java/org/exam/testclass.java
+++ src/main/java/org/exam/testclass.java
@@ -4,6 +4,6 @@ import com.github.javafaker.Faker;
 
 public class testclass {
     public static void main(String[] args) {
-System.out.println(Faker.instance().address().fullAddress());
+System.out.println(Faker.instance().address().city());
     }
 }
